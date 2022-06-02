11e29fc9af28728105dd66066858143bf8730b25
kiranphp13 <kiranphp13@gmail.com>
2022-06-02 22:50:06 +0530

    Add multiplication feature

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 """
 Unit tests for the calculator library
 """
-from calculator import (add, subtract)
+from calculator import (add, subtract, multiply)
 
 def test_add():
     result = add(4, 4)
@@ -11,3 +11,5 @@ def test_subtract():
     result = subtract(4, 4)
     assert result == 0
 
+def test_multiplication(self):
+    assert 100 == multiply(10, 3)
